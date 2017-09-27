c6ce5d3cf9198853ec06188880ca092805c10de9
hallgató <hallgat@LABOR115-02>
2017-09-27 09:39:55 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static bool isDivByDivisor(int[] A, inr divisor)
+        {
+                         for (int I = 0; if< A.Length; i++)
+            if (divNumber(A[int, 5)) return true;
+            return false;
+            // index of the element divisible by 5
+        }
+        static int indexOfDivisor(int[] A, int divisor))) return i;
+            return -1
+        static void Main(string[] args)
+        {
+            const int n = 10, range = 101;
+            int[] A = new int[n];
+            Random R = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(Array[])
+            }
+            static void bool divNumber(int number, int divisor)
+            }
+        static int sumUp(int [ Array)])
+
+                if (number % divisor
+            }
+            {
+                A[i] = R.Next(0, range);
+                          }
+            print(A);
+Console.Write(isDivByDivisor
+           int number 10, divisor = 5;
+            if (number % 5 ==)
+                Console.Write(divNumber(numver, dividirÍ));
+            ine su, = 0;
+          
+    {
+    //bool go = true;
+    return false;
+    if(divNumber (Array[IAppDomainSetup, 5))
+Console.Writeline("/n"A[i] + " is dividivle by 5))
+    //go = false;
+    break;
+        }
+    }
+}
